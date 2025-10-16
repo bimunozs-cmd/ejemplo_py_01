d44119822b135533867f9767de1690c77669a510
bimunozs-cmd <bi.munozs@duocuc.cl>
2025-10-16 18:20:44 -0300

    edades usurios y intento de lista

--- prueba.py/prueba.py
+++ prueba.py/prueba.py
@@ -1,18 +1,36 @@
 
+usuario = []
 
-
+edades = []
 
 print("hola soy nuevo")
 
 
-nombre = str(input("ingrese su nombre"))
+
+
+nombre = str(input("ingrese su nombre ="))
+
+usuario.append(nombre)
+
 
 
 saludar = print(f"hola {nombre} good day")
 
+edad = int(input(f"porfavor {nombre} ingresa tu edad ="))
+
+edades.append(edad)
 
 
 
+
+for usr in usuario:
+
+
+    print(usr)
+
+for ed in edades :
+
+    print(ed)
 
 
 
